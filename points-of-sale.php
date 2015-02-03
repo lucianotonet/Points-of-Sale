696ddf494bb6dcfc9194f5d10d9d46d0c85fb64b
Luciano <tonetlds@gmail.com>
2015-02-03 20:13:36 -0200

    Updated PLUGIN URL

--- points-of-sale.php
+++ points-of-sale.php
@@ -10,10 +10,10 @@
  * @wordpress-plugin
  * Plugin Name:       Points Of Sale
  * Plugin URI:        https://github.com/tonetlds/Points-of-Sale
- * Description:       Simple Google maps integration that display the points of sale where your costumers can buy your products.
+ * Description:       Simple Google maps integration to display the points of sale where your costumers can find your products. Filters by States, Cities and Neighborhoods.
  * Version:           1.0.0
  * Author:            Luciano Tonet
- * Author URI:        http://lucianotonet.com/
+ * Author URI:        https://github.com/tonetlds
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       points-of-sale
